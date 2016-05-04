6b0f0c08db847e58dcfb94049d2cceda00670a01
Nyan Of The Moon <nyanofthemoon@gmail.com>
2016-05-04 18:01:20 -0400

    removed unused react-redux

--- client/src/components/Row.jsx
+++ client/src/components/Row.jsx
@@ -1,5 +1,4 @@
 import React, {Component, PropTypes} from 'react'
-import {connect} from 'react-redux'
 
 import Tile from './Tile'
 
@@ -9,12 +8,12 @@ class Row extends Component {
             <div className="region__row">
                 {this.props.cells.map(function(cell, index) {
                     return (<Tile
-                        key        = {'tile-' + index}
-                        x          = {cell.getIn(['coordinates','x'])}
-                        y          = {cell.getIn(['coordinates','y'])}
-                        data       = {cell.getIn(['tile','data'])}
-                        node       = {cell.getIn(['node','data'])}
-                        items      = {cell.getIn(['items','data'])}
+                        key   = {'tile-' + index}
+                        x     = {cell.getIn(['coordinates','x'])}
+                        y     = {cell.getIn(['coordinates','y'])}
+                        data  = {cell.getIn(['tile','data'])}
+                        node  = {cell.getIn(['node','data'])}
+                        items = {cell.getIn(['items','data'])}
                     />);
                 })}
             </div>
@@ -22,21 +21,8 @@ class Row extends Component {
     }
 }
 
-Row.contextTypes = {
-    store: PropTypes.object.isRequired
-}
-
 Row.propTypes = {
-    cells     : PropTypes.object.isRequired,
-    player    : PropTypes.object.isRequired,
-    characters: PropTypes.object.isRequired
+    cells: PropTypes.object.isRequired
 }
 
-function mapStateToProps(state) {
-    return {
-        player    : state.player,
-        characters: state.characters
-    }
-}
-
-export default connect(mapStateToProps)(Row);
+export default Row;
--- client/src/containers/Region.jsx
+++ client/src/containers/Region.jsx
@@ -1,25 +1,19 @@
 import React, {Component, PropTypes} from 'react'
-import {bindActionCreators} from 'redux'
 import {connect} from 'react-redux'
 
 import Row from './../components/Row';
 
-import {queryPlayer, queryRegion, enterRegion, leaveRegion} from './../actions';
-
 class Region extends Component {
-    constructor(props) {
-        super(props);
-    }
     // @TODO Support maps of different sizes
     render() {
-        const {player, region, characters, actions} = this.props
+        const {region} = this.props
         return (
             <section className="region">
                 <div className={['cycle', 'cycle--' + region.get('cycle')].join(' ')}></div>
                 {region.get('rows').map(function(row, index) {
                     return (<Row
-                        key        = {'row-' + index}
-                        cells      = {row}
+                        key   = {'row-' + index}
+                        cells = {row}
                     />);
                 })}
             </section>
@@ -32,32 +26,13 @@ Region.contextTypes = {
 }
 
 Region.propTypes = {
-    player    : PropTypes.object.isRequired,
-    region    : PropTypes.object.isRequired,
-    characters: PropTypes.object.isRequired,
-    actions   : PropTypes.object.isRequired
+    region: PropTypes.object.isRequired
 }
 
 function mapStateToProps(state) {
     return {
-        player    : state.player,
-        region    : state.region,
-        characters: state.characters
+        region: state.region
     }
 }
 
-function mapDispatchToProps(dispatch) {
-    return {
-        actions: bindActionCreators({
-            queryPlayer,
-            queryRegion,
-            enterRegion,
-            leaveRegion
-        }, dispatch)
-    }
-}
-
-export default connect(
-    mapStateToProps,
-    mapDispatchToProps
-)(Region);
+export default connect(mapStateToProps)(Region);
